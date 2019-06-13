45527de5b11d35f6d0f2e41e79bc0f63f071d98b
Jose Trinidad <internick10@yahoo.com.mx>
2019-06-13 09:55:10 -0500

    first commit

---
+++ src/main/java/com/galosoft/MyDocumentWebApplication.java
@@ -0,0 +1,13 @@
+package com.galosoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDocumentWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDocumentWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galosoft/MyDocumentWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galosoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDocumentWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
